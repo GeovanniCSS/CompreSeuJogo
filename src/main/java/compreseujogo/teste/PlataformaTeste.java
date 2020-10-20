be8e274cb76e66ef26f951d30f394205c0be6dcf
pirees <leonardopires-1997@hotmail.com>
2020-10-20 15:50:21 -0300

    atualizacao

--- src/main/java/compreseujogo/teste/PlataformaTeste.java
+++ src/main/java/compreseujogo/teste/PlataformaTeste.java
@@ -2,28 +2,29 @@ package compreseujogo.teste;
 
 import compreseujogo.model.bo.PlataformaBo;
 import compreseujogo.model.entity.Plataforma;
-import compreseujogo.viacep.WebServiceCep;
 
 public class PlataformaTeste {
 
 	public static void main(String[] args) {
 		PlataformaBo pBo = new PlataformaBo();
-		Plataforma plataforma = new Plataforma(0,"Xbox",true,"");
-		WebServiceCep w = WebServiceCep.searchCep("83701030");
+		Plataforma plataforma = new Plataforma();
 		
-		w.getCidade();
+		plataforma.setCodigoSku("asdadsadd");
+		plataforma.setNome("PS4");
+		;
 		
-		System.out.println(w.getCidade());
 
-		/*try {
-			System.out.println(pBo.nova(plataforma, Plataforma.class));
+		try {
+			System.out.println(plataforma.getCodigoSku());
+			//System.out.println(pBo.nova(plataforma, Plataforma.class));
 			// System.out.println(pBo.desativarAtivar(plataforma));
+			System.out.println(pBo.saveOrUpdate(plataforma));
 			//pBo.listar("novo", plataforma).forEach(System.out::println);
 			;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}*/
+		}
 	}
 
 }
