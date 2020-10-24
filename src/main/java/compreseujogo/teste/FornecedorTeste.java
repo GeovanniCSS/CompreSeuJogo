d671d188e2d137cca45030c3cf0cb7dd1cf4626a
pirees <leonardopires-1997@hotmail.com>
2020-10-24 16:35:00 -0300

    commit para dar pull do Giovanni

--- src/main/java/compreseujogo/teste/FornecedorTeste.java
+++ src/main/java/compreseujogo/teste/FornecedorTeste.java
@@ -9,15 +9,15 @@ public class FornecedorTeste {
 	public static void main(String[] args) {
 		FornecedorBo fornecedorBo = new FornecedorBo();
 		Facade facade = new Facade();
-		Fornecedor fornecedor = new Fornecedor(0, "Catatau jogos", "28.833.271/0001-47", "catataujogos@gmail.com.br",
-				"Jardinete Roberto", "81330-432", "PR", "(41) 95445-9459", "catatau", true);
+		//Fornecedor fornecedor = new Fornecedor(0, "Catatau jogos", "28.833.271/0001-47", "catataujogos@gmail.com.br",
+				//"Jardinete Roberto", "81330-432", "PR", "(41) 95445-9459", "catatau", true);
 		
 		
 		try {	
 			//System.out.println(fornecedorBo.saveOrUpdate(fornecedor));
 			//fornecedorBo.list("", fornecedor).forEach(System.out::println);
 			//facade.listarFornecedorNome(fornecedor).forEach(System.out::println);
-			facade.inserirFornecedor(fornecedor);
+			//facade.inserirFornecedor(fornecedor);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
