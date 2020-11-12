9f615ebfdfb573d63a6fa531031e36e8f0d61e05
Geovanni Corsino <geovannicorsino@gmail.com>
2020-11-12 01:36:35 -0300

    Arrumando Menu e cadastro da imagem pro produto

--- src/main/java/compreseujogo/controller/ProdutoController.java
+++ src/main/java/compreseujogo/controller/ProdutoController.java
@@ -1,19 +1,20 @@
 package compreseujogo.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.Part;
-
-import org.primefaces.event.FileUploadEvent;
-import org.primefaces.model.file.UploadedFile;
 
 import compreseujogo.facade.Facade;
 import compreseujogo.model.entity.Categoria;
@@ -34,8 +35,8 @@ public class ProdutoController implements Serializable {
 	private List<Fornecedor> fornecedores;
 	private List<Marca> marcas;
 	private List<Plataforma> plataformas;
-	private UploadedFile file;
 	private Part arquivo;
+	private String destino;
 
 	public ProdutoController() {
 		this.produto = new Produto();
@@ -44,12 +45,14 @@ public class ProdutoController implements Serializable {
 		this.fornecedores = new ArrayList<Fornecedor>();
 		this.marcas = new ArrayList<Marca>();
 		this.plataformas = new ArrayList<Plataforma>();
+		this.destino = "C:\\\\temp\\\\WS-eclipse\\\\compreseujogo_3.0\\\\src\\\\main\\\\webapp\\\\resources\\\\imagem\\\\";
 	}
 
 	public String salvar() {
 		FacesContext context = FacesContext.getCurrentInstance();
 		Facade facade = new Facade();
 		try {
+			importa();
 			context.addMessage(null, new FacesMessage(facade.salvarProduto(this.produto), FacesMessage.FACES_MESSAGES));
 			return "listaProduto?faces-redirect=true";
 		} catch (Exception e) {
@@ -60,12 +63,55 @@ public class ProdutoController implements Serializable {
 
 	public void importa() {
 		FacesContext context = FacesContext.getCurrentInstance();
+
+		String conteudo = getFileName(arquivo.getName() + ".png");
+		produto.setImagem(conteudo);
+		context.addMessage(null, new FacesMessage("Salvou a imagem" + conteudo, FacesMessage.FACES_MESSAGES));
 		try {
-			String conteudo = new Scanner(arquivo.getInputStream()).useDelimiter("\\A").next();
-			context.addMessage(null, new FacesMessage("Salvou a imagem" +conteudo, FacesMessage.FACES_MESSAGES));
+			copyFile(conteudo, arquivo.getInputStream());
 		} catch (IOException e) {
 			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), ""));
 		}
+	}
+
+	public void copyFile(String fileName, InputStream in) {
+		try {
+			// write the inputStream to a FileOutputStream
+			OutputStream out = new FileOutputStream(new File(destino + fileName));
+			int read = 0;
+			byte[] bytes = new byte[1024];
+
+			while ((read = in.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+
+			in.close();
+			out.flush();
+			out.close();
+
+			System.out.println("Novo arquivo criado '" + fileName + "'!");
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public String getExtensao(String nomeArquivo) {
+		int posicaoPonto = nomeArquivo.lastIndexOf(".");
+		int tamanhoString = nomeArquivo.length();
+		return nomeArquivo.substring(posicaoPonto + 1, tamanhoString);
+	}
+
+	public String getFileName(String nomeArquivo) {
+		String data = "yyyy-MM-dd";
+		String hora = "HH-mm-ss-SSS";
+		String data1, hora1;
+
+		java.util.Date agora = new java.util.Date();
+		SimpleDateFormat formata = new SimpleDateFormat(data);
+		data1 = formata.format(agora);
+		formata = new SimpleDateFormat(hora);
+		hora1 = formata.format(agora);
+		return data1 + "-" + hora1 + "." + getExtensao(nomeArquivo);
 	}
 
 	public String crud() {
@@ -79,13 +125,6 @@ public class ProdutoController implements Serializable {
 
 	public void excluir(Produto produto) {
 		this.produto = produto;
-	}
-
-	public void upload(FileUploadEvent event) {
-		String nome = file.getFileName();
-		System.out.println(file.getFileName());
-		System.out.println(nome);
-		produto.setImagem(nome);
 	}
 
 	public Produto getProduto() {
@@ -169,14 +208,6 @@ public class ProdutoController implements Serializable {
 
 	public void setPlataformas(List<Plataforma> plataformas) {
 		this.plataformas = plataformas;
-	}
-
-	public UploadedFile getFile() {
-		return file;
-	}
-
-	public void setFile(UploadedFile file) {
-		this.file = file;
 	}
 
 	public Part getArquivo() {
