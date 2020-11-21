8c8cee0622d4752db3cdc480e48e2651396a3a14
Geovanni Corsino <geovannicorsino@gmail.com>
2020-11-21 19:02:00 -0300

    ADD(PessoaDao) login

--- src/main/java/compreseujogo/model/dao/PessoaDao.java
+++ src/main/java/compreseujogo/model/dao/PessoaDao.java
@@ -9,28 +9,34 @@ import compreseujogo.model.entity.EntityBase;
 import compreseujogo.model.entity.Pessoa;
 import compreseujogo.util.EntityManagerUtil;
 
-public class PessoaDao <T extends Pessoa > {
-	
+public class PessoaDao<T extends Pessoa> {
+
 	private static EntityManager em = EntityManagerUtil.getEntityManager();
-	
-	public List<T> list(String parameter,Pessoa pessoa ,Class<T> classe) {
+
+	public List<T> list(String parameter, Pessoa pessoa, Class<T> classe) {
 		Query q = null;
-		
+
 		if (parameter.equals("email")) {
-			q = em.createQuery("SELECT t FROM "+classe.getSimpleName().toString()+" t WHERE t.email = :email");
+			q = em.createQuery("SELECT t FROM Pessoa t WHERE t.email = :email");
 			q.setParameter("email", pessoa.getEmail());
 		} else if (parameter.equals("cpf")) {
-			q = em.createQuery("SELECT t FROM "+classe.getSimpleName().toString()+" t WHERE t.cpf = :cpf");
+			q = em.createQuery("SELECT t FROM Pessoa t WHERE t.cpf = :cpf");
 			q.setParameter("cpf", pessoa.getCpf());
 		} else if (parameter.equals("login")) {
-			q = em.createQuery("SELECT t FROM "+classe.getSimpleName().toString()+" t WHERE t.email = :email "
+			q = em.createQuery("SELECT t FROM " + classe.getSimpleName().toString() + " t WHERE t.email = :email "
 					+ "AND t.senha = :senha");
 			q.setParameter("email", pessoa.getEmail());
 			q.setParameter("senha", pessoa.getSenha());
 		} else if (parameter.equals("organizada")) {
-			q = em.createQuery("SELECT t FROM "+ classe.getSimpleName().toString()+" t ORDER BY t.id");
+			q = em.createQuery("SELECT t FROM " + classe.getSimpleName().toString() + " t ORDER BY t.nome");
 		}
 		return q.getResultList();
 	}
-	
+
+	public Pessoa login(Pessoa pessoa) {
+		Query q = em.createQuery("SELECT p FROM Pessoa p WHERE p.email = :email AND p.senha = :senha");
+		q.setParameter("email", pessoa.getEmail());
+		q.setParameter("senha", pessoa.getSenha());
+		return (Pessoa) q.getResultList().get(0);
+	}
 }
