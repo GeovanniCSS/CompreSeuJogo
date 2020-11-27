80f7a64fce768232e3930c1ac6e404b76032395b
Geovanni Corsino <geovannicorsino@gmail.com>
2020-11-26 22:05:10 -0300

    UPDATE(VendaDao) list
    Para selecionar todos itens de uma venda

--- src/main/java/compreseujogo/model/dao/ItemVendaDao.java
+++ src/main/java/compreseujogo/model/dao/ItemVendaDao.java
@@ -9,18 +9,23 @@ import compreseujogo.model.entity.ItemVenda;
 import compreseujogo.util.EntityManagerUtil;
 
 public class ItemVendaDao {
-  
-  private static EntityManager em = EntityManagerUtil.getEntityManager();
+
+	private static EntityManager em = EntityManagerUtil.getEntityManager();
 
 	public List<ItemVenda> list(String parameter, ItemVenda item) {
-     Query q = null;
-		 if (parameter.equals("Nota")) {
-			 q = em.createQuery("SELECT item FROM ItemVenda item WHERE item.venda.cliente.id = :cliente AND item.produto.id = :produto");
-			 q.setParameter("cliente", item.getVenda().getCliente().getId());
-			 q.setParameter("produto", item.getProduto().getId());
-          } else if (parameter.equals("Vendidos")) {
-			 q = em.createQuery("SELECT item FROM ItemVenda item INNER JOIN item.p");
-          }
+		Query q = null;
+		if (parameter.equals("Nota")) {
+			q = em.createQuery(
+					"SELECT item FROM ItemVenda item WHERE item.venda.cliente.id = :cliente AND item.produto.id = :produto");
+			q.setParameter("cliente", item.getVenda().getCliente().getId());
+			q.setParameter("produto", item.getProduto().getId());
+		} else if (parameter.equals("Vendidos")) {
+			q = em.createQuery("SELECT item FROM ItemVenda item INNER JOIN item.p");
+		} else if (parameter.equals("Venda")) {
+			q = em.createQuery("SELECT item FROM ItemVenda item WHERE item.venda.id = :venda");
+			q.setParameter("venda", item.getVenda().getId());
+		}
+
 		return q.getResultList();
-    }
+	}
 }
